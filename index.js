c3f115eb754167c1dc325def604b6582e6003da4
Sérgio Glórias <sergio@serginho.dev>
2022-01-13 19:15:24 +0000

    controlo de qualidade

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ const server = fastify({
     trustProxy: true
 });
 
-function linkRandom(geo = "") {
-    let num = (Math.random()).toFixed(3)
+function linkRandom(geo = "") { // this initializer has complexity = 13
+    let num = (Math.random()).toFixed(3);
 
-    if (["T1"].includes(geo)) return "https://youtu.be/SN5awaBMkuY"
-    else if (num <= 0.01) return "https://youtu.be/vfjnUgToHnA"
-    else if (num <= 0.15) return "https://youtu.be/XDFFvsXqJJk"
-    else if (num <= 0.25) return "https://youtu.be/v1POP-m76ac"
-    else if (num <= 0.30 && ["BR", "PT"].includes(geo)) return "https://youtube.com/clip/UgyI6DhZfLywdxJ8jN14AaABCQ"
-    else if (num <= 0.40) return "https://youtu.be/MO7bRMa9bmA?t=37"
-    else if (num <= 0.45) return "https://youtu.be/K7XHy8nppf4"
-    else if (0.666 === num && ["BR", "PT"].includes(geo)) return "https://youtu.be/wUHKpxtWvUQ"
-    else if (0.666 === num && !["BR", "PT"].includes(geo)) return "https://youtu.be/EzN9u2xBBVY?t=5"
-    else return "https://youtu.be/dQw4w9WgXcQ"
+    if (["T1"].includes(geo)) {return "https://youtu.be/SN5awaBMkuY";}
+    else if (num <= 0.01) {return "https://youtu.be/vfjnUgToHnA";}
+    else if (num <= 0.15) {return "https://youtu.be/XDFFvsXqJJk";}
+    else if (num <= 0.25) {return "https://youtu.be/v1POP-m76ac";}
+    else if (num <= 0.30 && ["BR", "PT"].includes(geo)) {return "https://youtube.com/clip/UgyI6DhZfLywdxJ8jN14AaABCQ";}
+    else if (num <= 0.40) {return "https://youtu.be/MO7bRMa9bmA?t=37";}
+    else if (num <= 0.45) {return "https://youtu.be/K7XHy8nppf4";}
+    else if (0.666 === num && ["BR", "PT"].includes(geo)) {return "https://youtu.be/wUHKpxtWvUQ";}
+    else if (0.666 === num && !["BR", "PT"].includes(geo)) {return "https://youtu.be/EzN9u2xBBVY?t=5";}
+    else {return "https://youtu.be/dQw4w9WgXcQ";}
 }
 
 server.register(fastifyStatic, {
     root: path.join(__dirname, "i"),
     prefix: "/i/"
 });
-server.register(fastifyFavicon)
+server.register(fastifyFavicon);
 
-server.get('/', async (request, reply) => {
+server.get("/", async (request, reply) => {
 
-    let link = linkRandom(request.headers['cf-ipcountry'])
+    let link = linkRandom(request.headers["cf-ipcountry"])
     let html = `<!DOCTYPE html>
 <meta charset="utf-8">
 <meta property="og:site_name" content="Um presente selvagem apareceu apareceu!">
@@ -43,7 +43,7 @@ server.get('/', async (request, reply) => {
 <meta property="og:image" content="/i/Nitro.png">
 <meta property="og:description" content="Expira em 48 horas">
 <meta name="theme-color" content="#FFFFFF">
-<title>>Você recebeu uma assinatura de presente!</title>
+<title>Você recebeu uma assinatura de presente!</title>
 <!-- Matomo -->
 <script>
   var _paq = window._paq = window._paq || [];
@@ -61,32 +61,32 @@ server.get('/', async (request, reply) => {
 </script>
 <!-- End Matomo Code -->
 <meta http-equiv="refresh" content="0; URL=${link}">
-<link rel="canonical" href="${link}">`
+<link rel="canonical" href="${link}">`;
 
-    return reply.type('text/html').send(html)
+    return reply.type("text/html").send(html);
 
-})
+});
 
-server.get("/:name", async (request, reply) => {
+server.get("/:name", async (request, reply) => { // this initializer has complexity = 9
 
-    let sec = escapeHTML(request.params.name)
-    if (sec.length > 50) return reply.status(400).send("Invalid link")
+    let sec = escapeHTML(request.params.name);
+    if (sec.length > 50) {return reply.status(400).send("Invalid link");}
 
-    let link
+    let link;
     switch (sec.toLocaleLowerCase()) {
         case "\ud83d\udc27":
         case "pinguim":
         case "pinguin":
         case "tux":
-            link = "https://youtu.be/vfjnUgToHnA"
+            link = "https://youtu.be/vfjnUgToHnA";
             break;
         case "ravena":
         case "raven":
         case "666":
-            link = "https://www.youtube.com/watch?v=qgbF4WI49jE"
+            link = "https://www.youtube.com/watch?v=qgbF4WI49jE";
             break;
         default:
-            link = linkRandom(request.headers['cf-ipcountry'])
+            link = linkRandom(request.headers["cf-ipcountry"]);
             break;
     }
 
@@ -117,20 +117,19 @@ server.get("/:name", async (request, reply) => {
 <meta http-equiv="refresh" content="0; URL=${link}">
 <link rel="canonical" href="${link}">`
 
-    return reply.type('text/html').send(html)
+    return reply.type('text/html').send(html);
 })
 
 server.setNotFoundHandler(function (request, reply) {
-    reply.redirect("/")
+    reply.redirect("/");
 })
 
 const start = async () => {
     try {
         await server.listen(3002, "0.0.0.0");
-        console.log(`server listening on ${server.server.address().port}`);
     } catch (err) {
-        console.log(err);
+        server.log.error(err);
         process.exit(1);
     }
-}
+};
 start();
